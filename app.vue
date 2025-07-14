7d8893dc5a036cd4a6a30bb5ac96137f5ed1d5e7
stepangordeev <stgordeev@gmail.com>
2025-07-14 12:09:16 -0500

    layout changes

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <script setup>
-  import { NConfigProvider, NButton, NLayout, NLayoutContent, NLayoutHeader, NLayoutFooter, NMenu } from 'naive-ui'
+  import { NConfigProvider, NButton, NLayout, NLayoutContent, NLayoutHeader, NLayoutFooter, NMenu, NGradientText, NFlex } from 'naive-ui'
 
   const counter = useState('counter', () => Math.round(Math.random() * 1000))
   const menuOptions = [
@@ -41,16 +41,33 @@
 
 <template>
   <n-layout>
-    <n-layout-header>
-      <n-menu
-        mode="horizontal"
-        :options="menuOptions"
-        responsive
-      />
+
+    <n-layout-header style="padding: 10px 20px;" bordered>
+      <n-flex justify="space-between" align-items="center">
+
+        <n-gradient-text type="primary" size="24">
+            Stepan Gordeev
+        </n-gradient-text>
+
+        <n-menu
+          mode="horizontal"
+          :options="menuOptions"
+          responsive
+        />
+        
+        <n-button quaternary type="default" tag="a" href="https://sgordeev.com">
+          Stepan Gordeev
+        </n-button>
+
+      </n-flex>
     </n-layout-header>
-        <n-layout-content content-style="padding: 24px;">
-          <NuxtPage />
-        </n-layout-content>
-        <n-layout-footer>Chengfu Road</n-layout-footer>
+
+    <n-layout-content content-style="padding: 24px;">
+
+      <NuxtPage />
+
+    </n-layout-content>
+
+    <n-layout-footer>Footer</n-layout-footer>
   </n-layout>  
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,3 @@
 <template>
-    <n-button>
-    +
-    </n-button>
+    Research
 </template>
